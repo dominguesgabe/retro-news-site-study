b00554ab11744af5d996114ae95a5adb64573cc8
dominguesgabe <gabrieldomingues522@gmail.com>
2022-09-02 23:34:09 -0300

    bulding mobile nav and starting css destructuring in scopes

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const menu = document.querySelector('#menu-toggle');
+const navbar = document.querySelector('.nav');
+let showing = true;
 
-menu.addEventListener('onClick', () => {
-    alert('oi')
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('mobile');
+    document.body.style.overflow = showing ? "hidden" : "initial";
+
+    showing = !showing;
 })
